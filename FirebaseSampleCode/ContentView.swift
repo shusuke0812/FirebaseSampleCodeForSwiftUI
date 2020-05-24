015eda5ac25dd84f1df3eff541539a9d2f4b7685
shusuke0812 <shu.ota0812@gmail.com>
2020-05-24 10:04:10 +0900

    初期コミット

---
+++ FirebaseSampleCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseSampleCode
+//
+//  Created by Shusuke Ota on 2020/5/24.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

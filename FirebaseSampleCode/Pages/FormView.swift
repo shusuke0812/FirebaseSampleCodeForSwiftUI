8eae300f5948b807d6d7104cd740a083e9923e85
shusuke0812 <shu.ota0812@gmail.com>
2020-05-24 17:02:41 +0900

    [add]利用規約リンク実装

--- FirebaseSampleCode/Pages/FormView.swift
+++ FirebaseSampleCode/Pages/FormView.swift
@@ -9,10 +9,23 @@
 import SwiftUI
 
 struct FormView: View {
+    
+    @State private var url = "https://docs.google.com/document/d/1xwKhNVNW-CUuRFp_jk8vhMG1TaihEtrzbEM9RPy77Ac/edit"
+    @State private var isPresented: Bool = false
+    
+    
     var body: some View {
-        Text("ATM情報投稿ページ")
+        Button(action: {
+            // print("ボタンがタップされました")
+            self.isPresented.toggle()
+        }) {
+            Text("利用規約")
+        }.sheet(isPresented: $isPresented) {
+            SafariView(url: URL(string: self.url)!)
+        }
     }
 }
+
 
 struct FormView_Previews: PreviewProvider {
     static var previews: some View {

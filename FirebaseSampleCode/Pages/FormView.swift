4a33ec02afa17654d4dfc741602a69b37167bc41
shusuke0812 <shu.ota0812@gmail.com>
2020-05-25 23:53:34 +0900

    [add]フォーム画面項目追加
    ・銀行種別選択ボタン（Picker）
    ・利用規約ボタンを画面下部へ移動

--- FirebaseSampleCode/Pages/FormView.swift
+++ FirebaseSampleCode/Pages/FormView.swift
@@ -13,15 +13,33 @@ struct FormView: View {
     @State private var url = "https://docs.google.com/document/d/1xwKhNVNW-CUuRFp_jk8vhMG1TaihEtrzbEM9RPy77Ac/edit"
     @State private var isPresented: Bool = false
     
+    @State private var atmIndex = 0
+    
+    var atms = ["ゆうちょ銀行", "みずほ銀行", "三菱東京UFJ", "三井住友銀行"]
     
     var body: some View {
-        Button(action: {
-            // print("ボタンがタップされました")
-            self.isPresented.toggle()
-        }) {
-            Text("利用規約")
-        }.sheet(isPresented: $isPresented) {
-            SafariView(url: URL(string: self.url)!)
+        VStack {
+            NavigationView {
+                Form {
+                    Section {
+                        Picker(selection: $atmIndex, label: Text("ATM種別")) {
+                            ForEach(0 ..< atms.count) {
+                                Text(self.atms[$0]).tag($0)
+                            }
+                        }
+                    }
+                }
+            }
+            Spacer()
+            Button(action: {
+                // print("ボタンがタップされました")
+                self.isPresented.toggle()
+            }) {
+                Text("利用規約")
+            }.sheet(isPresented: $isPresented) {
+                SafariView(url: URL(string: self.url)!)
+            }
+            Spacer()
         }
     }
 }

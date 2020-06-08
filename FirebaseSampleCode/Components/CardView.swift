b007d60ceb031d1bc3b26dff2308d5e51473c859
shusuke0812 <shu.ota0812@gmail.com>
2020-06-08 22:47:46 +0900

    [add]地図表示切り替えボタン追加

--- FirebaseSampleCode/Components/CardView.swift
+++ FirebaseSampleCode/Components/CardView.swift
@@ -13,17 +13,61 @@ import SDWebImageSwiftUI
 struct CardView: View {
     
     @ObservedObject var atms = getAtmData()
+    
     @Binding var searchedText: String
+    @Binding var listView: Bool
    
     // @State var favorite = false
     
     var body: some View {
-        VStack {
-            if self.searchedText != "" {
-                if self.atms.datas.filter({$0.atmKind.lowercased().contains(self.searchedText.lowercased())}).count == 0 {
-                    Text("該当するATMが見つかりませんでした")
+        ZStack(alignment: .bottomTrailing) {
+            VStack {
+                if self.searchedText != "" {
+                    if self.atms.datas.filter({$0.atmKind.lowercased().contains(self.searchedText.lowercased())}).count == 0 {
+                        Text("該当するATMが見つかりませんでした")
+                    } else {
+                        List(atms.datas.filter{$0.atmKind.lowercased().contains(self.searchedText.lowercased())}) {i in
+                            HStack {
+                                AnimatedImage(url: URL(string: i.iconImage)!)
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 60, height: 60)
+                                    .clipShape(Circle())
+                                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                                    .shadow(color: .gray, radius: 1, x:0, y:0)
+                                VStack(alignment: .leading) {
+                                    HStack {
+                                        Text(i.atmKind)
+                                        Spacer()
+                                        if i.favorite {
+                                            Image(systemName: "heart.fill")
+                                                .foregroundColor(.pink)
+                                                .padding(.trailing, 10)
+                                                .onTapGesture {
+                                                    self.atms.updateData(id: i.id, favorite: false)
+                                                }
+                                        } else {
+                                            Image(systemName: "heart")
+                                                .padding(.trailing, 10)
+                                                .onTapGesture {
+                                                    self.atms.updateData(id: i.id, favorite: true)
+                                                }
+                                        }
+                                        
+                                    }
+                                    Text(i.atmName)
+                                    Text(i.atmAddress)
+                                }
+                            }
+                            // カード型のUIに枠線を追加する
+                            //.overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
+                            //.frame(minWidth: 150, minHeight: 180)
+                            //.padding()
+                        }
+                    }
                 } else {
-                    List(atms.datas.filter{$0.atmKind.lowercased().contains(self.searchedText.lowercased())}) {i in
+                    List(atms.datas) {i in
                         HStack {
                             AnimatedImage(url: URL(string: i.iconImage)!)
                                 .renderingMode(.original)
@@ -63,53 +107,27 @@ struct CardView: View {
                         //.padding()
                     }
                 }
-            } else {
-                List(atms.datas) {i in
-                    HStack {
-                        AnimatedImage(url: URL(string: i.iconImage)!)
-                            .renderingMode(.original)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 60, height: 60)
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke(Color.gray, lineWidth: 2))
-                            .shadow(color: .gray, radius: 1, x:0, y:0)
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text(i.atmKind)
-                                Spacer()
-                                if i.favorite {
-                                    Image(systemName: "heart.fill")
-                                        .foregroundColor(.pink)
-                                        .padding(.trailing, 10)
-                                        .onTapGesture {
-                                            self.atms.updateData(id: i.id, favorite: false)
-                                        }
-                                } else {
-                                    Image(systemName: "heart")
-                                        .padding(.trailing, 10)
-                                        .onTapGesture {
-                                            self.atms.updateData(id: i.id, favorite: true)
-                                        }
-                                }
-                                
-                            }
-                            Text(i.atmName)
-                            Text(i.atmAddress)
-                        }
-                    }
-                    // カード型のUIに枠線を追加する
-                    //.overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
-                    //.frame(minWidth: 150, minHeight: 180)
-                    //.padding()
-                }
             }
-        }
-        .onAppear() {
-            self.atms.readData()
-            
-            // ListViewの区切り線を消す
-            // UITableView.appearance().separatorStyle = .none
+            .onAppear() {
+                self.atms.readData()
+                
+                // ListViewの区切り線を消す
+                // UITableView.appearance().separatorStyle = .none
+            }
+            Button(action: {
+                self.listView.toggle()
+            }) {
+                Text("地図表示")
+                    .foregroundColor(.blue)
+                    .frame(width: 80, height: 30, alignment: .center)
+                    .padding(.init(top: 10, leading: 16, bottom: 10, trailing:  16))
+                    //.overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 1))
+            }
+            .background(Color.white)
+            .cornerRadius(.infinity)
+            .padding()
+            .compositingGroup()
+            .shadow(color: Color.gray, radius: 2)
         }
     }
 }

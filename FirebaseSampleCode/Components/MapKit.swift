bb408224e9a06dce568798c1913af8d02cc00045
shusuke0812 <shu.ota0812@gmail.com>
2020-06-09 22:30:35 +0900

    [add]地図画面にて下記項目を追加
    ・ユーザーの現在地を表示させる機能を実装
    ・Info.plistにNSLocation系のkey, valueを設定

--- FirebaseSampleCode/Components/MapKit.swift
+++ FirebaseSampleCode/Components/MapKit.swift
@@ -11,12 +11,26 @@ import MapKit
 
 struct MapKit: UIViewRepresentable {
     
+        var locationManager = CLLocationManager()
+    
+        func setupManager() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestAlwaysAuthorization()
+        }
+    
         func makeUIView(context: Context) -> MKMapView {
-            let mapView = MKMapView(frame: .zero)
+            setupManager()
+            
+            let mapView = MKMapView(frame:  UIScreen.main.bounds)
+            
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
             return mapView
         }
         
         func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapKit>) {
+        //func updateUIView(_ uiView: MKMapView, context: Context) {
             let coordinate = CLLocationCoordinate2D(latitude: 35.7618662, longitude: 139.5363992)
             let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
             let region = MKCoordinateRegion(center: coordinate, span: span)
